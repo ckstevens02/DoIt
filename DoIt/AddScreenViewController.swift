6fccea72f723d4e6c81cb06251a156c443d86b32
my first commit <Christina@Christinas-MacBook-Pro.local>
2017-04-27 13:56:02 +0900

    add view controller for adding tasks — includes buttons, switches, and text fields.. connected to code

---
+++ DoIt/AddScreenViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddScreenViewController.swift
+//  DoIt
+//
+//  Created by Christina Stevens on 4/27/17.
+//  Copyright © 2017 Christina Stevens. All rights reserved.
+//
+
+import UIKit
+
+class AddScreenViewController: UIViewController {
+
+    @IBOutlet weak var taskName: UITextField!
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func addTaskButton(_ sender: Any) {
+    }
+
+
+}
